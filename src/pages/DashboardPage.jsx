d48c568e3c994a4653fd8be2738a6f897054473a
Sadia hassan <Sadia22hassan@gmail.com>
2025-08-16 13:00:11 +0500

    Update DashboardPage.jsx

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,9 +1,10 @@
 export default function Dashboard() {
   return (
     <div className=" text-black p-4 rounded">
-      <h1>Welcome to the Gym Dashboard</h1>
+      <h1>Welcome to the Web Guru Academy</h1>
       <p>This simple system lets you add members easily under the “Members” page.</p>
     </div>
   );
 }
+
 
